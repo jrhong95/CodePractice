5b42ba454aa9b4f329aa95cf1f887934e1e46932
jrhong95 <jrhong95@gmail.com>
2020-10-14 18:32:14 +0900

    Add 201014_10250.py

---
+++ Baekjoon/201014_10250.py
@@ -0,0 +1,10 @@
+# 10250번 ACM호텔
+num = int(input())
+
+for _ in range(num):
+    H, W, N = map(int, input().split())
+    room_cnt, floor = divmod(N, H)
+    if floor == 0:
+        floor = H
+        room_cnt -= 1
+    print(floor * 100 + room_cnt + 1)

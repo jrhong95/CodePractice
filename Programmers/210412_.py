ffafe59c152240e8cea4927a215ae9111bccead9
jrhong95 <jrhong95@gmail.com>
2021-04-12 23:25:53 +0900

    210412 프로그래머스

--- Programmers/210412_.py
+++ Programmers/210412_.py
@@ -2,4 +2,4 @@ import re
 
 
 def solution(skill, skill_trees):
-    return sum(1 for s in skill_trees if skill.startswith(''.join(re.compile(r'['+skill+']').findall(s))))
+    return sum(skill.startswith(''.join(re.compile(r'['+skill+']').findall(s))) for s in skill_trees)
